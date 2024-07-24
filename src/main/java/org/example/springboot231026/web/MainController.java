37ce3d599aba162118d07bfd9d514b606a744dc2
rnjstjddo88 <rnjstjddo88@naver.com>
2024-07-24 23:56:44 +0900

    분양글목록에서 글이 옆으로 보이지 않고 아래로 보이는 현상 해결 <div>잘못설정한게 원인

--- src/main/java/org/example/springboot231026/web/MainController.java
+++ src/main/java/org/example/springboot231026/web/MainController.java
@@ -13,13 +13,13 @@ public class MainController {
     @GetMapping("/main")
     public String main(){
         System.out.println("컨트롤러MainController main() 진입 html.html 이동 ");
-        return "home/home";
+        return "redirect:/home/home";
     }
 
     @GetMapping("/")
     public String basic(){
         System.out.println("컨트롤러MainController basic() 진입 /home/home 이동");
-        return "home/home";
+        return "redirect:/home/home";
     }
 
 
